787cb1102665def491ef084946f013bfac95d3cd
Brayden Fidler <bfidler28@yahoo.com>
2017-09-15 14:35:14 -0400

    TicTacToe

---
+++ TicTacToe/BoardPosition.java
@@ -0,0 +1,53 @@
+package TicTacToe;
+public class BoardPosition {
+
+    //fields
+    private int row;
+    private int col;
+    private char player;
+
+    //Constructor
+    /**
+     *
+     * @param r row of position
+     * @param c column of position
+     * @param playerChar character marker
+     * @requires (0 <= r <= 7) and (0 <= c <= 7) and
+     *  ((playerChar == 'X') or (playerChar == 'O'))
+     * @ensures [GameBoard functions that use BoardPosition as a parameter do
+     *  not break]
+     */
+    public BoardPosition(int r, int c, char playerChar) {
+
+        row = r;
+        col = c;
+        player = playerChar;
+    }
+
+    /**
+     *
+     *@return the row of the coordinate
+     */
+    public int getRow(){
+
+        return row;
+    }
+
+    /**
+     *
+     *@return the column of the coordinate
+     */
+    public int getColumn() {
+
+        return col;
+    }
+
+    /**
+     *
+     * @return the character of the player (X or O)
+     */
+    public char getPlayer() {
+
+        return player;
+    }
+}
